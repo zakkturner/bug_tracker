1b7d94d3d842f074b83c3454c9e9f6201d5bcec3
Zachary Turner <zakkturner@yahoo.com>
2023-03-01 20:57:33 -0500

    Started bug tracker app. Can add bugs to db

---
+++ Bug_tracker/Connection.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows;
+
+namespace Bug_tracker
+{
+    internal class Connection
+    {
+        private MySqlConnection conn;
+        private string server;
+        private string user;
+        private string pass;
+        private string db;
+        private string port;
+
+        public Connection() {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            db = "bug_list";
+            user = "root";
+            pass = "admin";
+            port = "3307";
+            string connectionString;
+            connectionString = "Data Source=" + server + ";Port=" + port + ";Database=" + db + ";User Id=" + user + ";Password=" + pass + ";SSL Mode=0";
+            conn = new MySqlConnection(connectionString);
+
+        }
+        public bool OpenConnection()
+        {
+            try
+            {
+                conn.Open();
+                return true;
+            }catch(MySqlException e)
+            {
+                switch (e.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server. Contact admin");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Invalid username/password.");
+                        break ;
+
+                }
+                return false;
+            }
+        }
+        public void CloseConnection()
+        {
+            conn.Close();
+        }
+        public MySqlConnection GetConnection()
+        {
+            return conn;
+        }
+    }
+}
---
+++ Bug_tracker/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Bug_tracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private string title, sql;
+        private DateTime date;
+        private Connection conn = new Connection();
+        private MySqlCommand command;
+        private void ButtonAddBug_Click(object sender, RoutedEventArgs e)
+        {
+            title = bugName.Text;
+            date = DateTime.Now;
+            if (!string.IsNullOrWhiteSpace(bugName.Text) && !bugList.Items.Contains(bugName.Text))
+            {
+                //bugList.Items.Add(new BugItem() { Title = bugName.Text, Date = DateTime.Now }); 
+                sql = "INSERT INTO bugs (Title, Date) VALUES ('" + title + "',STR_TO_DATE('" + DateTime.Now.ToString("MM/dd/yyyy") + "', '%d/%m/%Y'))";
+                if(conn.OpenConnection() == true)
+                {
+                    try
+                    {
+                        command = new MySqlCommand(sql, conn.GetConnection());
+                        object a = command.ExecuteScalar();
+                        
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+
+                    }
+                }
+            }
+        }
+
+        public class BugItem
+        {
+            public string Title { get; set; }
+            public DateTime Date { get; set; }
+        }
+    }
+}
